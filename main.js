22bbac75ca890920852641396a3c4cfb5f1046a9
salmanorak <salman.orak@gmail.com>
2019-06-19 03:52:39 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+var flights = document.getElementsByClassName('flight-summary')
+
+for (var i=0; i<flights.length;i++)
+
+flights[i].addEventListener('click', function(e){
+
+
+    if(e.currentTarget.parentElement.classList.contains('open')){
+        e.currentTarget.parentElement.classList.remove('open')
+    }else{
+        if(document.querySelectorAll('.open').length>0){
+            document.querySelectorAll('.open')[0].classList.remove('open')
+        }
+        e.currentTarget.parentElement.classList.add('open')
+        e.currentTarget.scrollIntoView({behavior: "smooth"});
+
+    }
+})
+
